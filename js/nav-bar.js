d952ab752c60706c241ec082560d68009f393e5b
vrodrigo91 <122231030+vrodrigo91@users.noreply.github.com>
2023-01-11 17:11:32 -0600

    Update nav-bar.js

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -4,13 +4,11 @@
   posEnd = 0;
 
   box.on('scroll', function(){
-    if ($(window).width() > 599) {
       var posStart = $(this).scrollTop();
 
       if (posStart > posEnd) nav.addClass('hidden');
       else nav.removeClass('hidden');
 
       posEnd = posStart;
-    }
   });
 })();
