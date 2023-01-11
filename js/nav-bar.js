44d1193c33c4aeab6483eb48d2bdfaf4507459e7
vrodrigo91 <122231030+vrodrigo91@users.noreply.github.com>
2023-01-11 11:02:34 -0600

    Update nav-bar.js

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -2,9 +2,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("nav-bar").style.top = "0";
+    document.getElementById("nav").style.top = "0";
   } else {
-    document.getElementById("nav-bar").style.top = "-50px";
+    document.getElementById("nav").style.top = "-50px";
   }
   prevScrollpos = currentScrollPos;
 }
